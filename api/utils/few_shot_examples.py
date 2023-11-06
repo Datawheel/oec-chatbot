22e26e0a8e594ab75b2ac25950e2f5b8e88fc38d
Alexandra <alexandra@datawheel.us>
2023-11-06 10:02:38 -0300

    modifies few_shot function

--- api/utils/few_shot_examples.py
+++ api/utils/few_shot_examples.py
@@ -1,14 +1,17 @@
 import json
-from typing import List
 
+from typing import List
+from os import getenv
+
+FEW_SHOT_PATH = getenv('FEW_SHOT_PATH')
 
 few_shot_examples = {}
-with open("data/few_shot_examples.json", "r") as f:
+with open(FEW_SHOT_PATH, "r") as f:
     few_shot_examples = json.load(f)
 
 
-def get_few_shot_example_messages(mode: str = "table_selection", scope="USA", n=-1) -> List[dict]:
-    examples = few_shot_examples.get(scope, {}).get(mode, [])
+def get_few_shot_example_messages(mode: str = "table_selection", n=-1) -> List[dict]:
+    examples = few_shot_examples.get("USA", {}).get(mode, [])
     if n > 0:
         examples = examples[:n]
     if n == 0:
