2d73a774231b0f315a3c90ba4a4a51b2620abc2f
Alexandra <alexandra@datawheel.us>
2023-11-16 22:14:23 -0300

    adds function to get dimension members from api

--- api/utils/tables_to_db.py
+++ api/utils/tables_to_db.py
@@ -1,5 +1,8 @@
-import pandas as pd
 import os
+import pandas as pd
+import requests
+import urllib.parse
+
 from sqlalchemy import create_engine
 from sentence_transformers import SentenceTransformer
 
@@ -14,7 +17,7 @@ conn = engine.connect()
 
 def embedding(dataframe, column):
     """
-    Creates embeddings
+    Creates embeddings for text in the passed column
     """
     model = SentenceTransformer('multi-qa-MiniLM-L6-cos-v1')
 
@@ -24,17 +27,57 @@ def embedding(dataframe, column):
     return dataframe
 
 
-
 def create_table():
-    engine.execute("CREATE TABLE IF NOT EXISTS tables.tables (table_name text, table_description text, embedding vector(384))") 
+    engine.execute("CREATE TABLE IF NOT EXISTS datausa_drilldowns.drilldowns (product_id text, product_name text, cube_name text, drilldown text, embedding vector(384))") 
     return
 
-df = pd.read_csv('/Users/alexandrabjanes/Datawheel/CODE/datausa-chat/tables.csv')
-print(df.head())
+
+def get_data_from_api(api_url):
+    try:
+        r = requests.get(api_url)
+        df = pd.DataFrame.from_dict(r.json()['data'])
+    except: raise ValueError('Invalid API url:', api_url)
+
+    return df
 
 
-df_embeddings = embedding(df, 'table_description')
+def get_api_params(api_url):
+    parsed_url = urllib.parse.urlparse(api_url)
+    query_params = urllib.parse.parse_qs(parsed_url.query)
+
+    cube = query_params.get('cube', [''])[0]
+    drilldown = query_params.get('drilldowns', [''])[0]
+
+    cube_name = cube.replace('+', ' ')
+    drilldown = drilldown.replace('+', ' ')
+
+    return cube_name, drilldown
+
+
+def load_data_to_db(api_url, measure_name):
+    cube_name, drilldown = get_api_params(api_url)
+    df = get_data_from_api(api_url=api_url)
+    
+    df.rename(columns={f"{drilldown}": "product_name", f"{drilldown} ID": "product_id"}, inplace=True)
+
+    df['cube_name'] = f"{cube_name}"
+    df['drilldown'] = f"{drilldown}"
+    df.drop(f"{measure_name}", axis=1, inplace=True)
+
+    print(df.head())
+
+    df_embeddings = embedding(df, 'product_name')
+    df_embeddings.to_sql('drilldowns', conn, if_exists='append', index=False, schema='datausa_drilldowns')
+
+    return
+
+
+print("Enter API url: ")
+api_url = input()
+print("Enter measure name: ")
+measure_name = input()
+#df = pd.read_csv('/Users/alexandrabjanes/Datawheel/CODE/datausa-chat/tables.csv')
+#print(df.head())
 
 create_table()
-
-df_embeddings.to_sql('tables', conn, if_exists='append', index=False, schema='datausa_tables')
+load_data_to_db(api_url, measure_name = measure_name)
