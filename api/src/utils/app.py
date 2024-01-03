28b74488e5c3142f9e9e89df14ec2d0aaa305284
Alexandra <alexandra@datawheel.us>
2024-01-03 16:53:15 -0300

    add api_url print

--- api/src/utils/app.py
+++ api/src/utils/app.py
@@ -14,6 +14,7 @@ def get_api(query):
     table_db_llm = request_tables_to_lm_from_db(query)
     v, m, c = get_api_params_from_lm(query, table_db_llm[0], model = 'gpt-4-1106-preview')
     api_url = api_build(table = table_db_llm[0], drilldowns = v, measures = m, cuts = c)
+    print("API:", api_url)
     data, df = api_request(api_url)
 
     response = agent_answer(df, query)
