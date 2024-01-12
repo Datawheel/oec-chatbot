5a7e80118fe4d8a182d630415ad61bd4cbbdb7fd
Alexandra <alexandra@datawheel.us>
2024-01-12 13:42:55 -0300

    modify agent prompt

--- api/src/utils/data_analysis/data_analysis.py
+++ api/src/utils/data_analysis/data_analysis.py
@@ -1,12 +1,8 @@
-import json
-import openai
 import os
-import pandas as pd
 
 from os import getenv
 from dotenv import load_dotenv
-from langchain.agents import create_pandas_dataframe_agent, initialize_agent, load_tools, Tool
-from langchain.agents.agent_types import AgentType
+from langchain.agents import create_pandas_dataframe_agent
 from langchain.chat_models import ChatOpenAI
 from langchain import OpenAI
 
@@ -28,16 +24,18 @@ def agent_answer(df, natural_language_query):
         ---------------\n
         {natural_language_query}
         \n---------------\n
-        Explain your reasoning to reach the answer.
         Take into consideration the data type and formatting of the columns.
         Its possible that any product/service or other variables the user is referring to appears with a different name in the dataframe. Explain this in your answer in a polite manner, but always trying to give an answer with the available data.
+        If you can't answer the question with the provided data, please answer with "I can't answer your question with the available data".
         Lets think it through step by step.
+        Do not write 'python' anywhere.
         """
     )
 
     llm = ChatOpenAI(model_name='gpt-4-1106-preview', temperature=0, openai_api_key=openai_api_key)
     agent =  create_pandas_dataframe_agent(llm, df, verbose=True)
     response = agent.run(prompt)
-    print("\nAgent answer:", response)
-    print("\n", type(response))
+    
+    if(response == "Agent stopped due to iteration limit or time limit."): response = "I can't answer your question with the available data."
+    
     return response
