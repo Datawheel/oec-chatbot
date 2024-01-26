52d95d83ed5ed34c1a0259ea183e9801464a3f40
Alexandra <alexandra@datawheel.us>
2024-01-25 23:27:38 -0300

    add scripts to load cubes and drilldowns to the db

---
+++ api/src/utils/cubes_to_db.py
@@ -0,0 +1,50 @@
+import os
+import pandas as pd
+
+from sqlalchemy import create_engine
+from sentence_transformers import SentenceTransformer
+
+
+POSTGRES_USERNAME = os.getenv('POSTGRES_USER')
+POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+POSTGRES_URL = os.getenv('POSTGRES_URL')
+POSTGRES_DATABASE = os.getenv('POSTGRES_DB')
+
+engine = create_engine('postgresql+psycopg2://{}:{}@{}:5432/{}'.format(POSTGRES_USERNAME,POSTGRES_PASSWORD,POSTGRES_URL,POSTGRES_DATABASE))
+conn = engine.connect()
+
+def embedding(dataframe, column):
+    """
+    Creates embeddings for text in the passed column
+    """
+    model = SentenceTransformer('multi-qa-MiniLM-L6-cos-v1')
+
+    model_embeddings = model.encode(dataframe[column].to_list())
+    dataframe['embedding'] = model_embeddings.tolist()
+
+    return dataframe
+
+
+def create_table():
+    engine.execute("CREATE TABLE IF NOT EXISTS datausa_tables.cubes (table_name text, table_description text, embedding vector(384))") 
+    return
+
+
+def load_data_to_db(df):
+
+    print(df.head())
+
+    df_embeddings = embedding(df, 'table_description')
+    df_embeddings.to_sql('cubes', conn, if_exists='append', index=False, schema='datausa_tables')
+
+    return
+
+
+df = pd.DataFrame()
+
+df["table_name"] = ["Data_USA_House_election"]
+df['table_description'] = ["Table 'Data_USA_House_election' contains House election data, including number of votes by candidate, party and state."]
+
+create_table()
+
+load_data_to_db(df)
