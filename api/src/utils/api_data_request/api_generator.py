340379a8785b689b3874537d6592c80e8c481a86
nspmx <nico.perezmundaca@gmail.com>
2024-02-21 16:24:50 -0300

    add llama selection on api_generator

--- api/src/utils/api_data_request/api_generator.py
+++ api/src/utils/api_data_request/api_generator.py
@@ -63,54 +63,90 @@ def get_api_components_messages(table):
 
     return message
 
+def get_model_author(model):
+    """
+    Identify Model Author for Model requestes
+    """
+    # List of posible nodels
+    models = {
+      "openai": ["gpt-3.5-turbo", "gpt-4","gpt-4-0125-preview", "gpt-4-1106-preview"],
+      "llama": ["llama2"]
+    }
+
+    if model in models.get("openai"):
+        author = "openai"
+    elif model in models.get("llama"):
+        author = "llama"
+    else:
+        author = None
+    
+    return author
 
 def get_api_params_from_lm(natural_language_query, table = None, model="gpt-4", top_matches=False):
     """
     Identify API parameters to retrieve the data
     """
-    max_attempts = 5
-    attempts = 0
+    model_author = get_model_author(model)
+    print('here', model, model_author)
 
     content = get_api_components_messages(table)
 
-    messages = [{
-        "role": "system",
-        "content": content
-    }]
+    # logic for openai models
+    if model_author == "openai":
+        max_attempts = 5
+        attempts = 0
 
-    messages.append({
-        "role": "user",
-        "content": natural_language_query
-    })
-    
-    while attempts < max_attempts:
-        try:
-            response = openai.ChatCompletion.create(
-                model="gpt-4",
-                messages=messages,
-                temperature=0
-                )
-        except openai.error.Timeout as e:
-            print(f"OpenAI API request timed out (attempt {attempts + 1}): {e}")
-        except openai.error.APIError as e:
-            print(f"OpenAI API returned an API Error (attempt {attempts + 1}): {e}")
-        except openai.error.APIConnectionError as e:
-            print(f"OpenAI API request failed to connect: {e}")
-        except openai.error.ServiceUnavailableError as e:
-            print(f"OpenAI API service unavailable: {e}")
-        else:
-            break
-        attempts += 1
-        time.sleep(1)
+        messages = [{
+            "role": "system",
+            "content": content
+        }]
 
-    output_text = response['choices'][0]['message']['content']
-    print("\nChatGPT response:", output_text)
-    params = extract_text_from_markdown_triple_backticks(output_text)
-    print("\nParameters:", params)
+        messages.append({
+            "role": "user",
+            "content": natural_language_query
+        })
+        
+        while attempts < max_attempts:
+            try:
+                response = openai.ChatCompletion.create(
+                    model=model,
+                    messages=messages,
+                    temperature=0
+                    )
+            except openai.error.Timeout as e:
+                print(f"OpenAI API request timed out (attempt {attempts + 1}): {e}")
+            except openai.error.APIError as e:
+                print(f"OpenAI API returned an API Error (attempt {attempts + 1}): {e}")
+            except openai.error.APIConnectionError as e:
+                print(f"OpenAI API request failed to connect: {e}")
+            except openai.error.ServiceUnavailableError as e:
+                print(f"OpenAI API service unavailable: {e}")
+            else:
+                break
+            attempts += 1
+            time.sleep(1)
 
-    variables = json.loads(params).get("variables")
-    measures = json.loads(params).get("measures")
-    cuts = json.loads(params).get("filters")
+        output_text = response['choices'][0]['message']['content']
+        print("\nChatGPT response:", output_text)
+        params = extract_text_from_markdown_triple_backticks(output_text)
+        print("\nParameters:", params)
+
+        variables = json.loads(params).get("variables")
+        measures = json.loads(params).get("measures")
+        cuts = json.loads(params).get("filters")
+    elif model_author == "llama":
+        url = "https://caleuche-ollama.datawheel.us/api/generate"
+        payload = {
+            "model": "llama2",
+            "prompt": content
+        }
+
+        response = requests.post(url, json=payload)
+
+        print(response.text)
+    else:
+        # logics: ask for model on the list, or use a default one
+        status = "bad status"
 
     return variables, measures, cuts
 
