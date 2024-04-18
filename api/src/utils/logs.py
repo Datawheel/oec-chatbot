882453a707e1fceaac59d22048eb7966adf8575a
Alexandra <alexandra@datawheel.us>
2024-04-18 16:39:48 -0400

    add create logs table function

--- api/src/utils/logs.py
+++ api/src/utils/logs.py
@@ -13,6 +13,12 @@ def generate_custom_id():
     return f"ID-{timestamp}-{random_chars}"
 
 
+def create_table(table_name, schema_name):
+    POSTGRES_ENGINE.execute(f"CREATE SCHEMA IF NOT EXISTS {schema_name}")
+    POSTGRES_ENGINE.execute(f"CREATE TABLE IF NOT EXISTS {schema_name}.{table_name} (query_id text, question text, api_url text, response text, created_on text, drilldowns text, measures text, cuts text, cube text, duration text)")
+    return
+
+
 def log_apicall(query, api_url, response, drilldowns, measures, cuts, cube, duration):
     ct = datetime.now()
     custom_id = generate_custom_id()
@@ -29,6 +35,8 @@ def log_apicall(query, api_url, response, drilldowns, measures, cuts, cube, dura
         "cube": cube.name,
         "duration": duration
     }
+    
+    create_table(LOGS_TABLE_NAME, SCHEMA_LOGS)
 
     insert_query = text("""
         INSERT INTO chat.logs (query_id, question, api_url, response, created_on, drilldowns, measures, cuts, cube, duration)
