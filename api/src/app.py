9d413a9b709eed8f472af9c6df88e223a2ac211c
Alexandra <alexandra@datawheel.us>
2024-04-17 22:16:24 -0400

    fix missing arguments

--- api/src/app.py
+++ api/src/app.py
@@ -11,20 +11,22 @@ def get_api(query, TABLES_PATH, step=None, **kwargs):
     if step == 'request_tables_to_lm_from_db':
         manager = TableManager(TABLES_PATH)
         table = request_tables_to_lm_from_db(query, manager)
-        return get_api(query, TABLES_PATH, step='get_api_params_from_lm', **{'table': table})
+        return get_api(query, TABLES_PATH, step='get_api_params_from_lm', **{'table': table, 'manager': manager})
         
     elif step == 'get_api_params_from_lm':
         table = kwargs['table']
+        manager = kwargs['manager']
         variables, measures, cuts = get_api_params_from_lm(query, table, model = 'gpt-4')
         api = api_build(table, manager, variables, measures, cuts)
-        api_url = api.build_url()
+        api_url = api.build_api()
         print("API:", api_url)
-        return get_api(query, TABLES_PATH, step='fetch_data', **{**kwargs, **{'api': api}})
+        return get_api(query, TABLES_PATH, step='fetch_data', **{**kwargs, **{'api': api, "api_url": api_url}})
 
     elif step == 'fetch_data': 
         api = kwargs['api']
+        api_url = kwargs['api_url']
         data, df, response = api.fetch_data()
-        return get_api(query, TABLES_PATH, step='agent_answer', **{**kwargs, **{'df': df, 'response': response, 'api_url': api_url}})
+        return get_api(query, TABLES_PATH, step='agent_answer', **{**kwargs, **{'df': df, 'response': response}})
 
     elif step == 'agent_answer':
         api = kwargs['api']
@@ -50,4 +52,4 @@ def get_api(query, TABLES_PATH, step=None, **kwargs):
 
 if __name__ == "__main__":
     TABLES_PATH = getenv('TABLES_PATH')
-    get_api('How much did the CPI of fresh fruits change between 2019 and 2021', TABLES_PATH)
+    get_api('How much coffee did Colombia export to Chile between 2010 and 2020?', TABLES_PATH)
