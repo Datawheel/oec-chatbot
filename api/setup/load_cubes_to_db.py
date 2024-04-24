e43dba9f87cd22f5b9c111c4a0b63b149f25cd6f
Alexandra <alexandra@datawheel.us>
2024-04-24 14:59:24 -0400

    modify setup scripts

--- api/setup/load_cubes_to_db.py
+++ api/setup/load_cubes_to_db.py
@@ -5,13 +5,12 @@ import sys
 
 from sentence_transformers import SentenceTransformer
 
-from src.config import POSTGRES_ENGINE, SCHEMA_TABLES, CUBES_TABLE_NAME
-from src.utils.similarity_search import embedding
+from config import POSTGRES_ENGINE, SCHEMA_TABLES, CUBES_TABLE_NAME, TABLES_PATH
+from utils.similarity_search import embedding
 
 table_name = CUBES_TABLE_NAME
 schema_name = SCHEMA_TABLES
 embedding_size = 384
-
 
 def create_table(table_name, schema_name, embedding_size = 384):
     POSTGRES_ENGINE.execute(f"CREATE SCHEMA IF NOT EXISTS {schema_name}")
@@ -20,23 +19,36 @@ def create_table(table_name, schema_name, embedding_size = 384):
 
 def load_data_to_db(df, table_name, schema_name):
     df_embeddings = embedding(df, 'table_description')
-    df_embeddings.to_sql(table_name, con=POSTGRES_ENGINE, if_exists='replace', index=False, schema=schema_name)
+    df_embeddings.to_sql(table_name, con=POSTGRES_ENGINE, if_exists='append', index=False, schema=schema_name)
     return
 
-with open('tables.json', 'r') as file:
-    cubes_data = json.load(file)
+def main(include_cubes=False):
+    with open(TABLES_PATH, 'r') as file:
+        cubes_json = json.load(file)
 
-cubes = []
+    create_table(table_name, schema_name)
 
-for cube in cubes_data["tables"]:
-    cube_info = {
-        "table_name": cube["name"],
-        "table_description": cube["description"]
-    }
-    cubes.append(cube_info)
+    cubes = []
 
-df = pd.DataFrame(cubes)
+    for cube in cubes_json["cubes"]:
+        cube_name = cube['name']
+        if include_cubes and cube_name not in include_cubes:
+            continue
+        cube_info = {
+            "table_name": cube["name"],
+            "table_description": cube["description"]
+        }
+        cubes.append(cube_info)
 
-create_table(table_name, schema_name)
-load_data_to_db(df, table_name, schema_name)
+    if not include_cubes:
+        user_input = input("Are you sure you want to upload all cubes? (y/n): ")
+        if user_input.lower() == 'y':
+            df = pd.DataFrame(cubes)
+            load_data_to_db(df, table_name, schema_name)
+        else:
+            pass
+
+if __name__ == "__main__":
+    include_cubes = False # if set to False it will ingest all the cubes in the schema.json
+    main(include_cubes)
 
