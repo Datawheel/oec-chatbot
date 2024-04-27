a5cabfd7d77c6e8b197668b64d3d89a352a0eca7
Felipe Ramos <framosmorale@student.unimelb.edu.au>
2024-04-26 22:29:06 -0400

    rremoving some datausa  legacy text

--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -7,16 +7,13 @@ import {
 import Chatbot from "@/components/chat/Chatbot";
 // import RelatedResults from "../../components/chat/RelatedResults";
 
-
-
-
 export default function ChatPage() {
   // initialize required variables
   
 
   return (
     <>
-      <NextSeo title="DataUSA Chat" />
+      <NextSeo title="OEC AI Chat" />
       <Container size="xl" py="xl">
         <Stack justify="flex-start" mt="20px">
           <Image src="/OEC-initials-logo.png" width={100} height={35} style={{display: "block", margin: "0 auto"}} alt='logo' />
